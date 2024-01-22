9273e645c68be7d55f05bf9973e2fb4ea117747c
skociuba <skociuba83@gmail.com>
2024-01-22 14:38:15 +0100

    add photos

--- components/SliderData.js
+++ components/SliderData.js
@@ -10,6 +10,10 @@ import manga10 from "../public/manga10.jpg"
 import manga11 from "../public/manga11.jpg"
 import manga12 from "../public/manga12.jpg"
 import manga13 from "../public/manga13.jpg"
+import manga14 from "../public/manga14.jpg"
+import manga15 from "../public/manga15.jpg"
+import manga16 from "../public/manga16.jpg"
+import manga17 from "../public/manga17.jpg"
 
 export const SliderData = [
   {
@@ -58,6 +62,22 @@ export const SliderData = [
   },
   {
     image: manga13,
+    portrait: true,
+  },
+  {
+    image: manga14,
+    portrait: true,
+  },
+  {
+    image: manga15,
+    portrait: true,
+  },
+  {
+    image: manga16,
+    portrait: true,
+  },
+  {
+    image: manga17,
     portrait: true,
   },
 ]
